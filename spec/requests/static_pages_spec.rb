3c7e5349e824fdee0c208ae20cb0af692eacbc72
Jerome Scott <durrowkells@eadfrith.local>
2013-02-08 10:44:51 +1300

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,52 +1,31 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	before { visit root_path } 
 
+  subject { page }
   let(:base_title) { "Beer Thinking" }
 
   describe "Home page" do
 
-    it "should have the h1 'Beer Thinking'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Beer Thinking')
-    end
+  	before { visit root_path }
 
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Beer Thinking")
-    end
+  	it { should have_selector('h1',    text: 'Beer Thinking') }
+    it { should have_selector('title', text: full_title('')) }
 
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-    end
   end
-
-
+  
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "#{base_title} | Help")
-    end
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "#{base_title} | About Us")
-    end
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
 end
