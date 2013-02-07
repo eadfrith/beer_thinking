281ef18491ad062f498b88b21b4e7991de7f2dd0
Jerome Scott <durrowkells@eadfrith.local>
2013-02-07 16:34:04 +1300

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,28 +2,51 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Beer Thinking" }
+
   describe "Home page" do
 
-    it "should have the content 'Beer Thinking'" do
+    it "should have the h1 'Beer Thinking'" do
       visit '/static_pages/home'
-      page.should have_content('Beer Thinking')
+      page.should have_selector('h1', :text => 'Beer Thinking')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Beer Thinking")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
+
   describe "Help page" do
 
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
-
 end
