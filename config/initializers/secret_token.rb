fd293ca2526d5a89cd64aeac9e5939f32382abd1
Jerome Scott <durrowkells@eadfrith.local>
2013-02-07 14:22:52 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeerThinking::Application.config.secret_token = '6d57a8dc6dd5e1e0841c1caf6bc09567d9d1273e8d5bec052b1a5c72df30eff52fae7ddfad0c85c81255780b8a2f6881ae37c225930b88f7b696ddeb893c048b'
