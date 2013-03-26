d13ee7f9472f907cf7e53c5f1658b071692f1efc
Jerome Scott <durrowkells@eadfrith.local>
2013-03-26 23:07:29 +1300

    more js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,18 +10,18 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
 //= require jquery_ujs
 //= require jquery-ui
 //= require jquery_nested_form
 //= require jquery.ui.datepicker
+//= require bootstrap
+//= require jquery
 //= require raphael
 //= require morris
 //= require recipe
 //= require sessions
 //= require static_pages
 //= require fermentation
-//= require bootstrap
 //= users
 
 
